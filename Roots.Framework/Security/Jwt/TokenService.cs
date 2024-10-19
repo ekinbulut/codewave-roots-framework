a5bf6e5d9291021046aa9bc0e9a5de4dc5e62701
Ekin Bulut <ekinbulut@gmail.com>
2024-10-20 01:17:47 +0300

    jwt token moved to here

---
+++ Roots.Framework/Security/Jwt/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Roots.Framework.Security.Jwt;
+
+public interface ITokenService
+{
+    string GenerateToken(string userId, string username);
+}
+
+public class TokenService : ITokenService
+{
+    private readonly string _secretKey;
+    private readonly string _issuer;
+    private readonly string _audience;
+
+    public TokenService(string secretKey, string issuer, string audience)
+    {
+        _secretKey = secretKey;
+        _issuer = issuer;
+        _audience = audience;
+    }
+
+    public string GenerateToken(string userId, string username)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+                new Claim(ClaimTypes.Name, username)
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Issuer = _issuer,
+            Audience =  _audience
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
